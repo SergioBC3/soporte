a8fdbd6478c6dd5f9ce57eee92f760a0c57c9726
SergioBC3 <145582956+SergioBC3@users.noreply.github.com>
2023-11-08 10:24:00 -0500

    Add files via upload

---
+++ src/test/java/com/java/crud/demo/JavaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.crud.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
